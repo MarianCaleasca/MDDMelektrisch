6248a099b5d65477da36009ef93b6c88ded685a7
MarianCaleasca <mddmelektrisch@gmail.com>
2025-05-26 22:57:43 +0200

    Primo commit

--- Server.js
+++ Server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
     .then(() => console.log("Connesso a MongoDB"))
     .catch(err => console.error("Errore connessione MongoDB:", err));
 // JavaScript source code
