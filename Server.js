da403cdbf4c5494d9912e9ac77b72bf7a1f30b63
MarianCaleasca <mddmelektrisch@gmail.com>
2025-05-26 23:24:28 +0200

    Fix: rimozione doppia dichiarazione mongoose

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGO_URI;
+//const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
     .then(() => console.log("Connesso a MongoDB"))
